e09c476c013f5b1b717c606d089134870f70e163
iaashish <ippiliaashish@gmail.com>
2017-04-10 01:31:18 -0500

    Layout commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get  '/signup',  to: 'users#new'
+
   get 'static_pages/home'
 
-  get 'static_pages/help'
+  get 'help', to: 'static_pages#help'
 
   root 'static_pages#home'
 end
