63d3287203626da205744bc246182ab5c46bf295
iaashish <ippiliaashish@gmail.com>
2017-04-10 17:53:46 -0500

    re commit previous files

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,9 +9,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-<<<<<<< HEAD
-=======
-    
->>>>>>> filling-in-layout
+
   end
 end
