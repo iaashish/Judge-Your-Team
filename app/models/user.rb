7c13bb70e45c890418c973b7d770712db2f6a971
iaashish <ippiliaashish@gmail.com>
2017-04-10 23:48:07 -0500

    Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ before_save { email.downcase! }
                     format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
+	
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-
-
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
-
-  get 'static_pages/home'
-
-  get 'help', to: 'static_pages#help'
-  get  'about', to: 'static_pages#about'
-  get  'contact', to: 'static_pages#contact'
-  root 'static_pages#home'
-  resources :users
+	
+	root 'static_pages#home'
+  	get 'help', to: 'static_pages#help'
+	get 'contact', to: 'static_pages#contact'
+	get 'about', to: 'static_pages#about'
+	get '/signup',  to: 'users#new'
+	post '/signup',  to: 'users#create'
+	get  '/login',   to: 'sessions#new'
+	post '/login',   to: 'sessions#create'
+	delete '/logout',  to: 'sessions#destroy'
+	resources :users
 
 end
