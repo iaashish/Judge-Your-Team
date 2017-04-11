d069da98c1ae2f69b29f5d298998dd0efec4acdc
iaashish <ippiliaashish@gmail.com>
2017-04-10 21:03:13 -0500

    user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   get 'static_pages/home'
 
@@ -9,5 +10,6 @@ Rails.application.routes.draw do
   get  'about', to: 'static_pages#about'
   get  'contact', to: 'static_pages#contact'
   root 'static_pages#home'
+  resources :users
 
 end
