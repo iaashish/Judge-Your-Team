3cce13fce7907bcd3b77875b77ffcd1b3601b300
iaashish <ippiliaashish@gmail.com>
2017-04-16 01:36:10 -0500

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ end
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
 gem 'pg'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'fog'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,10 +5,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "aashish",
-                                         email: "ippiliaashish@gmail.com",
-                                         password:              "aashish11",
-                                         password_confirmation: "aashish11" } }
+      post users_path, params: { user: { name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar"} }
     end
 	follow_redirect!
     assert_template 'users/new'
